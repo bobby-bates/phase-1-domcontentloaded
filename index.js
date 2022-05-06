2e041cff63d99fc7f06a5623584cfa68112a81b1
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-05 21:12:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// document.addEventListener('DOMContentLoaded', () => {
+//     let text = document.querySelector('#text').textContent
+//     console.log(`text: ${text}`)
+//     return text = 'This is really cool!'
+//     console.log(`text: ${text}`)
+//     console.log('Text should have changed')
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(`text: ${document.querySelector('#text').textContent}`)
+    document.querySelector('#text').textContent = 'This is really cool!'
+    console.log(`text: ${document.querySelector('#text').textContent}`)
+})
